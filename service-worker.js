3a89ec37d4bca1eb4323f7486833aec23c22a8d4
Ezrael Vitto <ezravitto@gmail.com>
2026-09-14 19:43:33 +0800

    Grid mode

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Bump this whenever the cached files change, so old caches get cleared out.
-const CACHE_NAME = "focuz-cache-v3";
+const CACHE_NAME = "focuz-cache-v6";
 
 const APP_SHELL = [
   "./index.html",
