710e44d7af86f87f91fbd662053495e8928a1c11
Ezrael Vitto <ezravitto@gmail.com>
2026-09-15 09:52:02 +0800

    Add app button dash border update

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Bump this whenever the cached files change, so old caches get cleared out.
-const CACHE_NAME = "focuz-cache-v11";
+const CACHE_NAME = "focuz-cache-v12";
 
 const APP_SHELL = [
   "./index.html",
