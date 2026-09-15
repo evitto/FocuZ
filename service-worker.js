0d6ea7691c44ef8c10e36d47867a625fc91fc178
Ezrael Vitto <ezravitto@gmail.com>
2026-09-15 08:06:29 +0800

    Grid mode Welcome/Hello centered
    on hover - Slides to left and search bar appears

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Bump this whenever the cached files change, so old caches get cleared out.
-const CACHE_NAME = "focuz-cache-v8";
+const CACHE_NAME = "focuz-cache-v9";
 
 const APP_SHELL = [
   "./index.html",
